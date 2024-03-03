edb28957ac90aed8b50aba7611c7e9e3055bb0ac
yac0928 <kangarooinapark@gmail.com>
2024-03-03 16:01:33 +0800

    feat: ai chatroom, getExpensesByMonth & postExpense done

--- src/components/ChatRoom/ChatRoom.jsx
+++ src/components/ChatRoom/ChatRoom.jsx
@@ -23,20 +23,38 @@ function ChatRoom (isAuthenticated) {
         setMessages(outputMessages)
       }
       setUserInput('')
-      console.log(messages)
     } catch (error) {
       console.error('Error submitting user input:', error)
     }
   }
 
   useEffect(() => {
-    if (typeof messages === 'object' && messages.name === 'getExpenses' && messages.content) {
+    if (typeof messages === 'object' && messages.name === 'getExpenses') {
       const args = JSON.parse(messages.arguments)
       const { year, month, day } = args
       const params = { year, month, day, categoryId: null }
       navigate(
           `/expenses?year=${params.year}&month=${params.month}&day=${params.day}&categoryId=`,
           { state: { params } }
+      )
+    } else if (typeof messages === 'object' && messages.name === 'getExpensesByMonth') {
+      const args = JSON.parse(messages.arguments)
+      const { year, month } = args
+      const params = { year, month, categoryId: null }
+      navigate(
+        `/expenses-by-month?year=${params.year}&month=${params.month}&categoryId=`,
+        { state: { params } }
+      )
+    } else if (typeof messages === 'object' && messages.name === 'postExpense') {
+      const argsDate = JSON.parse(messages.arguments).date
+      console.log(argsDate)
+      const year = argsDate.substring(0, 4)
+      const month = argsDate.substring(5, 7)
+      const day = argsDate.substring(8, 10)
+      const params = { year, month, day, categoryId: null }
+      navigate(
+        `/expenses?year=${params.year}&month=${params.month}&day=${params.day}&categoryId=`,
+        { state: { params } }
       )
     }
   }
@@ -61,9 +79,35 @@ function ChatRoom (isAuthenticated) {
             : (
             <div className="message">
               {messages.role === 'function' && (
-                <div className={`message ${messages.role}`}>
-                  {messages.content}
-                </div>
+                  <div className={`message ${messages.role}`}>
+                    {
+                      (JSON.parse(messages.content).expenses && JSON.parse(messages.content).expenses.length > 0) || Object.keys(JSON.parse(messages.content).newExpenses).length > 0
+                        ? (
+                            JSON.parse(messages.content).expenses && JSON.parse(messages.content).expenses.length > 0
+                              ? (
+                                  JSON.parse(messages.content).expenses.map((expense, index) => (
+                                <div key={expense.id}>
+                                  <p>{index + 1}. Name: {expense.name}</p>
+                                  <p>Amount: {expense.amount}</p>
+                                </div>
+                                  ))
+                                )
+                              : (
+                              <div>
+                                {
+                                  <div>
+                                    <p>Date: {JSON.parse(messages.content).newExpenses.date.split('T')[0]}</p>
+                                    <p>Name: {JSON.parse(messages.content).newExpenses.name}</p>
+                                    <p>Amount: {JSON.parse(messages.content).newExpenses.amount}</p>
+                                  </div>
+                                }
+                              </div>
+                                )
+                          )
+                        : <p>No expenses to display.</p>
+                    }
+
+                  </div>
               )}
             </div>
               )}
