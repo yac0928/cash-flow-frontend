ef35dab9b75af312408d52f4bae375cc4bf17eb9
yac0928 <kangarooinapark@gmail.com>
2024-03-05 15:03:45 +0800

    feat: switch baseURL to product

--- src/apis/axiosConfig.js
+++ src/apis/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// const baseURL = 'https://cash-flow-api.zeabur.app/api'
-const baseURL = 'http://localhost:3000/api'
+const baseURL = 'https://cash-flow-api.zeabur.app/api'
+// const baseURL = 'http://localhost:3000/api'
 const axiosInstance = axios.create({ baseURL })
 
 axiosInstance.interceptors.request.use(
